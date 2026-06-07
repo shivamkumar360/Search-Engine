817f613c344ac599212d80dbce45d3a2a714a6c7
Shivam Kumar <shivamankuri123.com>
2026-06-07 10:24:27 +0000

    formatting

--- filecopy.cpp
+++ filecopy.cpp
@@ -1,34 +1,34 @@
-#include<iostream>
-#include<fstream>
-#include<sstream>
+#include <iostream>
+#include <fstream>
+#include <sstream>
 #include <unordered_map>
 #include <set>
-#include<cctype>
+#include <cctype>
 #include <vector>
-#include<algorithm>
+#include <algorithm>
 #include <iomanip>
-#include<cmath>
+#include <cmath>
 #include <filesystem>
 using namespace std;
 namespace fs = std::filesystem;
 
 class SearchEngine
 {
-    private:
-    unordered_map<string,unordered_map<string,int>>index;
-    set<string>irrelevent={"of","the","is","are","and"};
-    vector<string>files;
-    int totaldoc=0;
-    bool build=true;
+private:
+    unordered_map<string, unordered_map<string, int>> index;
+    set<string> irrelevent = {"of", "the", "is", "are", "and"};
+    vector<string> files;
+    int totaldoc = 0;
+    bool build = true;
 
-
-    void scan_directory() {
+    void scan_directory()
+    {
         files.clear();
-        if (!fs::exists("data")) 
+        if (!fs::exists("data"))
         {
-        return;
+            return;
         }
-        for (auto& datafile : fs::directory_iterator("data"))
+        for (auto &datafile : fs::directory_iterator("data"))
         {
             if (datafile.path().extension() == ".txt")
             {
@@ -37,56 +37,50 @@ class SearchEngine
         }
     }
 
-
-
-
-    static bool comp(pair<string,double>a,pair<string,double>b)
-{
-    if(a.second>b.second)
+    static bool comp(pair<string, double> a, pair<string, double> b)
     {
-        return true;
+        if (a.second > b.second)
+        {
+            return true;
+        }
+        return false;
     }
-    return false;
-}
-void indexsize(set<string>& docs)
-{
-    for(auto &p : index)
-{
-    for(auto &q : p.second)
+    void indexsize(set<string> &docs)
     {
-        docs.insert(q.first);
+        for (auto &p : index)
+        {
+            for (auto &q : p.second)
+            {
+                docs.insert(q.first);
+            }
+        }
     }
-}
-}
 
 public:
+    void initialize()
+    {
+        scan_directory();
 
-void initialize() 
-{
-        scan_directory(); 
-        
-        if (fs::exists("index.txt")) {
+        if (fs::exists("index.txt"))
+        {
             load_index();
         }
 
         // If  files changed, rebuilding of  index will take place
 
         set<string> docs;
-        indexsize(docs);    // indexsize function will push file names from existing index 
-        if (docs.size() != files.size()) // if no of files not  matched rebuilding will take place 
+        indexsize(docs);                 // indexsize function will push file names from existing index
+        if (docs.size() != files.size()) // if no of files not  matched rebuilding will take place
         {
             cout << "Index outdated or missing. Building now...\n";
             build_index();
-        } 
-        else 
+        }
+        else
         {
             cout << "Index loaded successfully from file.\n";
             totaldoc = docs.size();
         }
     }
-
-
-
 
     void build_index()
     {
@@ -94,62 +88,56 @@ void initialize()
         totaldoc = files.size();
         ofstream fout("index.txt");
 
-        for(auto &c:files)
-    {
-    ifstream fin;
-    fin.open(c);
-    if(!fin)
-    {
-        cout<<"error opening file\n";
-        return ;
-    }
-
-
-    string line;
-    while(getline(fin,line))
-    {
-        string clean="";
-        for(int i=0;i<line.length();i++)
+        for (auto &c : files)
         {
-            if(isalnum(line[i]) || line[i]==' ')
+            ifstream fin;
+            fin.open(c);
+            if (!fin)
             {
-                clean+=tolower(line[i]);
+                cout << "error opening file\n";
+                return;
             }
-            else
+
+            string line;
+            while (getline(fin, line))
             {
-                clean+=' ';
+                string clean = "";
+                for (int i = 0; i < line.length(); i++)
+                {
+                    if (isalnum(line[i]) || line[i] == ' ')
+                    {
+                        clean += tolower(line[i]);
+                    }
+                    else
+                    {
+                        clean += ' ';
+                    }
+                }
+                stringstream ss(clean);
+                string word;
+                while (ss >> word)
+                {
+                    if (irrelevent.find(word) != irrelevent.end())
+                    {
+                        continue;
+                    }
+                    index[word][c]++;
+                }
             }
         }
-        stringstream ss(clean);
-        string word;
-        while(ss>>word)
+
+        for (auto &idx : index)
         {
-            if(irrelevent.find(word)!=irrelevent.end())
-        {
-            continue;
+            fout << idx.first << " ";
+            for (auto &sec : idx.second)
+            {
+                fout << sec.first << ":" << sec.second << " ";
+            }
+
+            fout << "\n";
         }
-            index[word][c]++;
-        }
+        fout.close();
     }
-    
-    
-}
-
-for (auto& idx : index)
-{
-    fout << idx.first << " ";
-    for (auto& sec : idx.second) 
-    {
-        fout << sec.first << ":" << sec.second << " ";
-    }
-
-            
-    fout << "\n";
-
-    }
-    fout.close();
-
-}
 
     void load_index()
     {
@@ -157,157 +145,137 @@ for (auto& idx : index)
         ifstream fin("index.txt");
         if (!fin)
         {
-         return;
+            return;
         }
 
         index.clear();
         string l;
-        while(getline(fin,l))
+        while (getline(fin, l))
         {
-        stringstream ss(l);
-        string wd;
-        ss>>wd;
-        string te=wd;
-        while(ss>>wd)
-        {
-           int pos=wd.find(':');
-           if(pos == string::npos)
-           {
-            continue;
-           }
-           string file=wd.substr(0,pos);
-           int freq=stoi(wd.substr(pos + 1));
-           index[te][file]=freq;
-        }
-
+            stringstream ss(l);
+            string wd;
+            ss >> wd;
+            string te = wd;
+            while (ss >> wd)
+            {
+                int pos = wd.find(':');
+                if (pos == string::npos)
+                {
+                    continue;
+                }
+                string file = wd.substr(0, pos);
+                int freq = stoi(wd.substr(pos + 1));
+                index[te][file] = freq;
+            }
         }
         fin.close();
     }
-      
-
-
 
     void search(string query)
     {
 
-         string clean_query="";
+        string clean_query = "";
 
-
-        for(auto& q:query)
-    {
-        if(isalnum(q) || q==' ')
+        for (auto &q : query)
         {
-            clean_query+=tolower(q);
+            if (isalnum(q) || q == ' ')
+            {
+                clean_query += tolower(q);
+            }
+            else
+            {
+                clean_query += " ";
+            }
         }
-        else
+
+        // now query is converted into tokens of words
+
+        vector<string> token;
+        stringstream s(clean_query);
+        string to;
+        while (s >> to)
         {
-            clean_query+=" ";
+            if (irrelevent.find(to) != irrelevent.end())
+            {
+                continue;
+            }
+            token.push_back(to);
         }
-    }
 
-    // now query is converted into tokens of words 
-
-    vector<string>token;
-    stringstream s(clean_query);
-    string to;
-    while(s>>to)
-    {
-        if(irrelevent.find(to)!=irrelevent.end())
+        if (token.empty())
         {
-            continue;
+            cout << "no valid query \n";
+            return;
         }
-        token.push_back(to);
+
+        // idf storage
+
+        unordered_map<string, double> tokenidf;
+        for (auto &st : token)
+        {
+            if (index.find(st) != index.end())
+            {
+                tokenidf[st] = (log(1.00 + (double)totaldoc / index[st].size()));
+            }
+            else
+            {
+                tokenidf[st] = 0;
+            }
+        }
+
+        // file scores calculation
+
+        unordered_map<string, double> file_score;
+
+        for (auto &st : token)
+        {
+            if (index.find(st) != index.end())
+            {
+                for (auto &p : index[st])
+                {
+
+                    double tf = 1.00 + log10(p.second);
+                    file_score[p.first] += tf * tokenidf[st];
+                }
+            }
+        }
+
+        // storing files with rank
+
+        vector<pair<string, double>> filerank;
+        for (auto &p : file_score)
+        {
+            filerank.push_back({p.first, p.second});
+        }
+        sort(filerank.begin(), filerank.end(), comp);
+
+        cout << fixed << setprecision(2);
+        int cnt = 0;
+        for (auto &p : filerank)
+        {
+            if (cnt >= 5)
+            {
+                break;
+            }
+            cnt++;
+            cout << p.first << "-> " << p.second << endl;
+        }
     }
-
-    if(token.empty())
-{
-    cout << "no valid query \n";
-    return ;
-}
-
-
-    // idf storage
-
-
-unordered_map<string, double>tokenidf;
-for(auto& st:token)
-{
-    if(index.find(st)!=index.end())
-    {
-        tokenidf[st]=(log(1.00+(double)totaldoc/index[st].size()));
-    }
-    else
-    {
-         tokenidf[st]=0;
-    }
-}
-
-// file scores calculation
-
-
-unordered_map<string,double>file_score;
-
-for(auto& st:token)
-{
-    if(index.find(st)!=index.end())
-    {
-    for(auto& p:index[st])
-    {
-
-        double tf = 1.00 + log10(p.second); 
-        file_score[p.first] += tf * tokenidf[st];
-       
-}
-}
-}
-
-
-
-// storing files with rank
-
-
-vector<pair<string,double>>filerank;
-for(auto &p : file_score)
-{
-    filerank.push_back({p.first,p.second});
-}
-sort(filerank.begin(),filerank.end(),comp);
-
-
-cout << fixed << setprecision(2);
-int cnt=0;
-for(auto &p : filerank)
-{
-    if(cnt>=5)
-    {
-        break;
-    }
-    cnt++;
-    cout<<p.first<<"-> "<<p.second<<endl;
-}
-
-
-
-
-
-
-
-}
-
 };
 
-int main() {
+int main()
+{
     SearchEngine engine;
-    engine.initialize(); 
+    engine.initialize();
 
     string q;
-    while (true) 
+    while (true)
     {
         cout << "\nEnter Query (or 'exit'): ";
         getline(cin, q);
-        if (q == "exit") 
+        if (q == "exit")
         {
-        break;
+            break;
         }
         engine.search(q);
     }
